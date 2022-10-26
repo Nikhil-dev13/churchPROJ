763631035027d46f07d50b9118b2b44bb4ce6bad
nikhil-chacko <nikhil13012000@gmail.com>
2022-10-26 19:57:01 +0530

    udpate deploy changes

--- church/settings.py
+++ church/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-import dj_database_url
+from dotenv import load_dotenv
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,8 +121,13 @@ if DEBUG:
         }
     }
 
-DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+import dj_database_url
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
