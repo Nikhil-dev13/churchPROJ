2511a2ca8945d50adfc77d0882b5be575640b5b6
nikhil-chacko <nikhil13012000@gmail.com>
2022-10-29 10:44:53 +0530

    update setttings

--- church/settings.py
+++ church/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -166,6 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -202,4 +204,3 @@ SIMPLE_JWT = {
 # import django_heroku
 
 # django_heroku.settings(locals())
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
